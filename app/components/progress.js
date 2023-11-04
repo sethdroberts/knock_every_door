187881ceb5d9c8f3144a3ee76dee84e6a684b607
Seth Roberts <sethknock@gmail.com>
2023-11-04 03:18:36 +0000

    Fixed UI issue with progress bar

--- app/components/progress.js
+++ app/components/progress.js
@@ -1,7 +1,7 @@
 export function ProgressLabel() {
     return (
     
-    <div class="relative mb-5 h-5 rounded-full bg-gray-200">
+    <div class="relative mb-5 mt-10 h-5 rounded-full bg-gray-200">
         <div class=" mb-5 h-full animate-pulse rounded-full bg-blue-500" style={{width: "7%"}}>
             <span class="absolute inset-0 flex items-center justify-center text-xs font-semibold text-black">4% (of 4,611,640 homes)</span>
         </div>
